be42727c4df6383534e8f309c936f79743b744f3
NUR <nur.sub.73@gmail.com>
2021-02-04 20:54:03 +0900

    Add UsersRestrant class

---
+++ src/models/UsersRestaurant.java
@@ -0,0 +1,63 @@
+package models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Table(name = "usersrestaurants")
+@NamedQueries({
+    @NamedQuery(
+            name = "getMyAllUsersRestaurants",
+            query = "SELECT ur FROM UsersRestaurant AS ur WHERE ur.user = :user"
+    ),
+    @NamedQuery(
+            name = "getMyUsersRestaurantsCount",
+            query = "SELECT COUNT(ur) FROM UsersRestaurant AS ur WHERE ur.user = :user"
+        )
+})
+@Entity
+public class UsersRestaurant {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    private Restaurant restaurant;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Restaurant getRestaurant(){
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant){
+        this.restaurant = restaurant;
+    }
+}
